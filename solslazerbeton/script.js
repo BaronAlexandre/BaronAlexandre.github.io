519fb1bcb96522ef4f1f8aed0d0aebf8b6b65536
AzureAD\AlexandreBARON <abaron@hellowork.com>
2024-04-11 13:30:26 +0200

    start

--- solslazerbeton/script.js
+++ solslazerbeton/script.js
@@ -55,3 +55,45 @@ function handleSwipe() {
         console.log(nextRadio);
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll('.card');
+
+    // Générer la modal
+    const modal = document.createElement('div');
+    modal.id = 'modal';
+    modal.className = 'modal';
+    modal.innerHTML = `
+      <span class="close-btn">&times</span>
+      <img id="modal-img" class="modal-content" src="" alt="Image en grande taille">
+    `;
+    document.body.appendChild(modal);
+
+    cards.forEach(card => {
+        card.addEventListener('click', function () {
+            if (getComputedStyle(card).zIndex === '1') {
+                const imgSrc = this.querySelector("img").src.replace("/reduce","");
+                document.getElementById('modal-img').src = imgSrc;
+                modal.style.display = 'block';
+            }
+        });
+    });
+
+    // Fonction pour récupérer le bouton radio actif
+    function getActiveRadioButton() {
+        return [...document.querySelectorAll('input[type="radio"]')].find(radio => radio.checked);
+    }
+
+    // Fermer la modal en cliquant sur le bouton de fermeture
+    document.querySelector('.close-btn').addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Fermer la modal en cliquant en dehors de la modal
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
